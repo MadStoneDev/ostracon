ac6ecf85a07ea90fd388eb0d1f4bb405f6ca0d8a
Richard H. <richard@haddads.net.au>
2024-11-17 00:08:26 +1000

    setup supabase

---
+++ src/app/error/page.tsx
@@ -0,0 +1,12 @@
+﻿export default function ErrorPage() {
+  return (
+    <main
+      className={`flex-grow flex flex-col h-full`}
+      style={{
+        paddingTop: "60px",
+      }}
+    >
+      <p>Sorry, something went wrong</p>
+    </main>
+  );
+}
---
+++ src/middleware.ts
@@ -0,0 +1,65 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
+import { createClient } from "@/utils/supabase/client";
+
+export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  console.log(path);
+
+  // Public Routes
+  const publicRoutes = [
+    "/",
+    "/login",
+    "/register",
+    "/info",
+    "/privacy-policy",
+    "/terms-and-conditions",
+    "/cookies-policy",
+    "/error",
+  ];
+
+  // Skip for static files and API routes
+  if (
+    path.startsWith("/_next") ||
+    path.startsWith("/api") ||
+    path.includes(".")
+  ) {
+    return NextResponse.next();
+  }
+
+  // Skip for public routes
+  if (publicRoutes.includes(path)) {
+    return NextResponse.next();
+  }
+
+  // Temporary for development
+  return NextResponse.next();
+
+  // Check if user is authenticated
+  try {
+    const supabase = createClient();
+    const { data } = await supabase.auth.getUser();
+
+    if (!data) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return await updateSession(request);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.redirect(new URL("/error", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
